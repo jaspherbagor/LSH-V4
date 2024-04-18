fa696eaadc669e644b856e4c90710bf1b7525feb
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-18 20:22:06 +0800

    added total review for the customer admin and changed the card icon backgound color

--- lsh_capstone/app/Http/Controllers/Customer/CustomerHomeController.php
+++ lsh_capstone/app/Http/Controllers/Customer/CustomerHomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\AccommodationRate;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,9 +14,9 @@ class CustomerHomeController extends Controller
     {
         $total_completed_orders = Order::where('status','Completed')->where('customer_id',Auth::guard('customer')->user()->id)->count();
         $total_pending_orders = Order::where('status','Pending')->where('customer_id',Auth::guard('customer')->user()->id)->count();
-
         $recent_orders = Order::where('customer_id', Auth::guard('customer')->user()->id)->orderBy('id', 'desc')->skip(0)->take(5)->get();
-        return view('customer.home', compact('total_completed_orders','total_pending_orders', 'recent_orders'));
+        $total_reviews = AccommodationRate::where('customer_id',Auth::guard('customer')->user()->id)->count();
+        return view('customer.home', compact('total_completed_orders','total_pending_orders', 'recent_orders', 'total_reviews'));
 
     }
 }
