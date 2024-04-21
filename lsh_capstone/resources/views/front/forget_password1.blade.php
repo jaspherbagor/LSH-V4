069ed0edbd2995e2b6859564a5a1c7f0c3f8c268
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-21 13:33:07 +0800

    updated the structure of the  content of the forget password page

--- lsh_capstone/resources/views/front/forget_password1.blade.php
+++ lsh_capstone/resources/views/front/forget_password1.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		
         <meta name="description" content="">
-        <title>Labason Safe Haven Login</title>        
+        <title>Labason Safe Haven Forget Password</title>        
 		
         <link rel="icon" type="image/png" href="{{ asset('uploads/'.$global_setting_data->favicon) }}">
 
@@ -22,20 +22,20 @@
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
     </head>
-    <body class="login-body-container px-2 d-flex align-items-center justify-content-center">
+    <body class="forget-password-body-container px-2 d-flex align-items-center justify-content-center">
 
         
         <div class="page-content container-fluid">
             <div class="container">
                 <div class="row justify-content-center">
-                    <div class="col-md-4 py-4 px-4 login-container">
+                    <div class="col-md-4 py-4 px-4 forget-password-container">
                         <div class="text-center">
                             <a href="{{ route('home') }}">
                                 <img src="{{ asset('uploads/logo.png') }}" alt="" class="logo">
                             </a>
                         </div>
-                        <h2 class="text-center mb-4 mt-3">Login Account</h2>     
-                        <form action="{{ route('customer_login_submit') }}" method="post">
+                        <h2 class="text-center mb-4 mt-3">Forget Password</h2>     
+                        <form action="" method="post">
                             @csrf
                             <div class="login-form">
                                 <div class="mb-3">
@@ -45,22 +45,12 @@
                                         <span class="text-danger">{{ $errors->first('email') }}</span>
                                     @endif
                                 </div>
-                                <div class="mb-3">
-                                    <label for="" class="form-label">Password</label>
-                                    <input type="password" class="form-control form-input" name="password">
-                                    @if($errors->has('password'))
-                                        <span class="text-danger">{{ $errors->first('password') }}</span>
-                                    @endif
-                                </div>
                                 <div class="mb-4 text-center">
                                     <button type="submit" class="btn btn-primary bg-website">Login</button>
                                     
                                 </div>
                                 <div class="mt-2">
-                                    <a href="{{ route('customer_forget_password') }}" class="primary-color">Forget password.</a>
-                                    <br>
-                                    Don't have an account yet?
-                                    <a href="{{ route('customer_signup') }}" class="primary-color">Register instead.</a>
+                                    <a href="{{ route('customer_signin') }}" class="primary-color">Back to Login</a>
                                 </div>
                             </div>
                         </form>
