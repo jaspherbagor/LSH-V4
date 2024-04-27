4c8a1ed61b13c86bfb61ad59b4b43a803b170150
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-27 16:02:32 +0800

    added comments on the codes of faq controller

--- lsh_capstone/app/Http/Controllers/Front/FaqController.php
+++ lsh_capstone/app/Http/Controllers/Front/FaqController.php
@@ -1,16 +1,22 @@
 <?php
-
+// Namespace declaration: specifies that this controller belongs to the 'App\Http\Controllers\Front' namespace
 namespace App\Http\Controllers\Front;
 
-use App\Http\Controllers\Controller;
-use App\Models\Faq;
-use Illuminate\Http\Request;
+// Importing necessary classes and models
+use App\Http\Controllers\Controller; // Importing base controller class
+use App\Models\Faq; // Importing the Faq model for interacting with FAQ data
+use Illuminate\Http\Request; // Importing the Request class
 
+// Controller for handling FAQ-related requests
 class FaqController extends Controller
 {
+    // Method to display the list of FAQs
     public function index()
     {
+        // Retrieve all FAQs from the database
         $faq_all = Faq::get();
-        return view('front.faq',compact('faq_all'));
+        
+        // Render the 'front.faq' view and pass the list of FAQs to it
+        return view('front.faq', compact('faq_all'));
     }
 }
