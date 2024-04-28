2eddb39dd6f5f18dd0c938b6c5818bfb25f32579
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-28 18:51:16 +0800

    added comments in each lines of the codes in the photo controller in the front-end

--- lsh_capstone/app/Http/Controllers/Front/PhotoController.php
+++ lsh_capstone/app/Http/Controllers/Front/PhotoController.php
@@ -1,16 +1,22 @@
 <?php
-
+// Namespace declaration: specifies that this controller belongs to the 'App\Http\Controllers\Front' namespace
 namespace App\Http\Controllers\Front;
 
-use App\Http\Controllers\Controller;
-use App\Models\Photo;
-use Illuminate\Http\Request;
+// Importing necessary classes and models
+use App\Http\Controllers\Controller; // Importing base controller class
+use App\Models\Photo; // Importing the Photo model for interacting with photo data
+use Illuminate\Http\Request; // Importing the Request class
 
+// Controller for handling photo gallery-related requests
 class PhotoController extends Controller
 {
+    // Method to display a paginated list of photos in the gallery
     public function index()
     {
+        // Retrieve photos from the database and paginate them (12 photos per page)
         $photo_all = Photo::paginate(12);
-        return view('front.photo_gallery',compact('photo_all'));
+        
+        // Render the 'front.photo_gallery' view and pass the paginated photos to it
+        return view('front.photo_gallery', compact('photo_all'));
     }
 }
