7cdcdf5393fcf2d2d4afe68958e92f1ccf49209c
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-05-04 19:55:19 +0800

    updated the placeholder for checkin and checkout in the home cart submit

--- lsh_capstone/resources/views/front/home.blade.php
+++ lsh_capstone/resources/views/front/home.blade.php
@@ -44,7 +44,7 @@
                 </div>
                 <div class="col-lg-3">
                     <div class="form-group">
-                        <input type="text" name="checkin_checkout" class="form-control daterange1" placeholder="Checkin & Checkout">
+                        <input type="text" name="checkin_checkout" class="form-control daterange1" placeholder="Arrival & Departure">
                     </div>
                 </div>
                 <div class="col-lg-2">
