1db5da86d3db17cd9d5a377f688fc960eb2c94c3
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-05-07 22:00:03 +0800

    added a primary structure of  the apartment card

--- lsh_capstone/resources/views/admin/home.blade.php
+++ lsh_capstone/resources/views/admin/home.blade.php
@@ -106,6 +106,23 @@
             </div>
         </a>
     </div>
+    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+        <a href="{{ route('admin_subscriber_show') }}">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-website">
+                    <i class="fa fa-users"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Apartments</h4>
+                    </div>
+                    <div class="card-body">
+                        3
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
 </div>
 
 <div class="row">
