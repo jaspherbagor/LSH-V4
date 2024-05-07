bc331d1adc89eac09330511135a01bbc824accba
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-05-07 22:01:05 +0800

    added a partial structure of the hotel card in the admin dashboard

--- lsh_capstone/resources/views/admin/home.blade.php
+++ lsh_capstone/resources/views/admin/home.blade.php
@@ -123,6 +123,26 @@
             </div>
         </a>
     </div>
+    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+        <a href="{{ route('admin_subscriber_show') }}">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-website">
+                    <i class="fa fa-users"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Hotels</h4>
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+
+
+
 </div>
 
 <div class="row">
